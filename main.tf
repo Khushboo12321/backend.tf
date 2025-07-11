f0b2891028e9497fc76e02a6adbfcf607d15bb46
Khushboo12321 <khushboovora2414@gmail.com>
2025-07-11 21:24:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_app_service_plan" "deepcodr-firstplan" {
+  name = "firstapp-plan"
+  resource_group_name = "RG-CTB40-AZ-400-3"
+  location = "westus"
+
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-web-app"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_app_service" "Khush-testapp" {
+  name = "Khush-testapp"
+  resource_group_name = "RG-CTB40-AZ-400-3"
+  location = "westus"
+  app_service_plan_id = azurerm_app_service_plan.deepcodr-firstplan.id
+
+  site_config {
+    python_version = "3.4"
+    scm_type = "LocalGit"
+  }
+}
+
+resource "azurerm_storage_account" "khushstoragetest" {
+  name = "khushstoragetest"
+  resource_group_name = "RG-CTB40-AZ-400-3"
+  location = "westus"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
 }
